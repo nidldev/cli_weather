4e53aa03909a51e8b2fa8c1d8a23fa7accd1ee6b
Maxime Dublanc <dev@nidl-web.com>
2020-11-11 16:59:58 +0100

    Add initial script

---
+++ weather_script.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<?php
+
+define("OPENWEATHERMAP_APP_ID", "e4e79efe30717b8814947c44240b1310");
+
+$searchInput = "";
+if (isset($argv[1])) {
+  $searchInput = $argv[1];
+} else {
+  $searchInput = trim(fgets(STDIN));
+}
+
+$url = "api.openweathermap.org/data/2.5/weather?q=" . $searchInput .
+  "&appid=" . OPENWEATHERMAP_APP_ID . "&units=metric";
+
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_HEADER, 0);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($curl, CURLOPT_VERBOSE, 0);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+$response = json_decode(curl_exec($curl));
+
+curl_close($curl);
+
+$output = ucfirst($response->weather[0]->description) . ', ' .
+  round($response->main->temp) . ' degrees celsius';
+
+echo $output;
